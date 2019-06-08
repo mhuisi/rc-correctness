164dcf7edf65f730059e016301f6ee5f1ba6ea85
mhuisi <mhuisi@protonmail.com>
2019-06-08 17:03:21 +0200

    first draft of typing rules (app rules may need revamping later)

--- src/rc_correctness.lean
+++ src/rc_correctness.lean
@@ -49,6 +49,16 @@ instance fn_to_rc : has_coe fn rc := ⟨rc.fn⟩
 inductive lin_type : Type 
     | 𝕆 | 𝔹 | ℝ
 
+@[derive decidable_eq]
+inductive ob_lin_type : Type
+    | 𝕆 | 𝔹
+
+instance ob_lin_type_to_lin_type : has_coe ob_lin_type lin_type := ⟨λ oblt, 
+    match oblt with
+    | ob_lin_type.𝕆 := lin_type.𝕆
+    | ob_lin_type.𝔹 := lin_type.𝔹
+    end⟩ 
+
 open lin_type
 
 structure typed_rc := (c : rc) (ty : lin_type)
@@ -58,11 +68,14 @@ structure typed_var := (x : var) (ty : lin_type)
 instance typed_var_eq : decidable_eq typed_var := by tactic.mk_dec_eq_instance
 
 notation x ` ∶ `:2 τ := typed_var.mk x τ
+notation xs ` [∶] `:2 τ := xs.map (∶ τ)
 notation c ` ∷ `:2 τ := typed_rc.mk c τ 
 
 abbreviation type_context := multiset typed_var
 
 notation Γ ` ⊪ `:1 xs := ↑xs ≤ Γ
+
+structure param_typing := (Γ : type_context) (x : var) (β : ob_lin_type)
 
 inductive linear : type_context → typed_rc → Type
 notation Γ ` ⊩ `:1 t := linear Γ t
@@ -95,5 +108,34 @@ notation Γ ` ⊩ `:1 t := linear Γ t
 | case_b (Γ : type_context) (x : var) (Fs : list fn_body) :
     (Γ ⊪ [x ∶ 𝔹]) → (∀ F ∈ Fs, Γ ⊩ ↑F ∷ 𝕆)
     → (Γ ⊩ fn_body.case x Fs ∷ 𝕆)
+-- the app rules may need to get revamped down the road 
+-- (properly modelling β may prove to be difficult, and right now there are no restrictions on β).
+-- the current app rules are merely placeholders, for now. 
+-- maybe the correct design decision will be obvious once we start working with these rules!
+| const_app_full (pts : list param_typing) (c : const) :
+    (∀ pt ∈ pts, (pt : param_typing).Γ ⊩ pt.x ∷ pt.β)
+    → (multiset.join (pts.map (param_typing.Γ)) ⊩ expr.const_app_full c (pts.map (param_typing.x)) ∷ 𝕆)
+| const_app_part (ys : list var) (c : const) :
+    ys [∶] 𝕆 ⊩ expr.const_app_part c ys ∷ 𝕆
+| var_app (x y : var) :
+    [x ∶ 𝕆, y ∶ 𝕆] ⊩ expr.var_app x y ∷ 𝕆
+| cnstr_app (ys : list var) (i : ctor) :
+    ys [∶] 𝕆 ⊩ expr.ctor_app ⟨i, ys⟩ ∷ 𝕆
+| reset (x : var) :
+    [x ∶ 𝕆] ⊩ expr.reset x ∷ ℝ
+| reuse (x : var) (ys : list var) (i : ctor) :
+    [x ∶ ℝ] + (ys [∶] 𝕆) ⊩ expr.reuse x ⟨i, ys⟩ ∷ 𝕆
+| let_o (Γ : type_context) (xs : list var) (e : expr) (Δ : type_context) (z : var) (F : fn_body) :
+    (Γ ⊪ xs [∶] 𝔹) → (Γ ⊩ e ∷ 𝕆) → (Δ ⊪ (xs [∶] 𝕆) ++ [z ∶ 𝕆]) → (Δ ⊩ F ∷ 𝕆)
+    → (Γ - (xs [∶] 𝔹) + Δ - [z ∶ 𝕆] ⊩ fn_body.«let» z e F ∷ 𝕆)
+| let_r (Γ : type_context) (xs : list var) (e : expr) (Δ : type_context) (z : var) (F : fn_body) :
+    (Γ ⊪ xs [∶] 𝔹) → (Γ ⊩ e ∷ 𝕆) → (Δ ⊪ (xs [∶] 𝕆) ++ [z ∶ ℝ]) → (Δ ⊩ F ∷ 𝕆)
+    → (Γ - (xs [∶] 𝔹) + Δ - [z ∶ ℝ] ⊩ fn_body.«let» z e F ∷ 𝕆)
+| proj_bor (Γ : type_context) (x y : var) (F : fn_body) (i : ctor) :
+    (Γ ⊪ [x ∶ 𝔹, y ∶ 𝔹]) → (Γ ⊩ F ∷ 𝕆)
+    → (Γ - [y ∶ 𝔹] ⊩ fn_body.«let» y (expr.proj i x) F ∷ 𝕆)
+| proj_own (Γ : type_context) (x y : var) (F : fn_body) (i : ctor) :
+    (Γ ⊪ [x ∶ 𝕆, y ∶ 𝕆]) → (Γ ⊩ F ∷ 𝕆)
+    → (Γ - [y ∶ 𝕆] ⊩ fn_body.«let» y (expr.proj i x) (fn_body.inc y F) ∷ 𝕆)
 
 end rc_correctness
